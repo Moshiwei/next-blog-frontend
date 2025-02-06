60a83562f33e43155b7e3b2398ae2a2acd1aef26
Moshiwei <1194317170@qq.com>
2025-02-06 21:55:28 +0800

    Fix login page only direct to github

--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -6,7 +6,7 @@ export default function SignIn() {
     <form
       action={async () => {
         "use server"
-        await signIn("github")
+        await signIn()
       }}
     >
       <button className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"type="submit">
